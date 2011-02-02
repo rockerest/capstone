4655107a768ab3d0d44763798304d4726536c7e4
StuartFeldt <smfy83@mail.missouri.edu>
2011-02-01 19:18:39 -0600

    Login capabbility, session items

--- components/Header.php
+++ components/Header.php
@@ -2,7 +2,9 @@
 	set_include_path('backbone:components:content:scripts:styles');
 	
 	require_once('Template.php');
-
+	require_once('Session.php');
+	setSession(1,1);
+	
 	class Header
 	{
 		
@@ -21,8 +23,28 @@
 		{
 			$tmpl = new Template();
 			$tmpl->curr = $this->cur_page;
-
-			$tmpl->menu = array(
+			
+			$tmpl->menu = $_SESSION['active'] ? $_SESSION['role']==1 ? 
+						array(
+						"display" => array(
+										"Home",
+										"Menu",
+										"Pay",
+										"OrderList",
+										"DBTEST",
+										"Item Upload",
+										"Logout"
+										),
+						"link" => array(
+										"index.php",
+										"order.php",
+										"checkout.php",
+										"orderlist.php",
+										"dbtest.php",
+										"uploadForm.php",
+										"login.php?logout=true"
+										)
+						) :  array(
 						"display" => array(
 										"Home",
 										"Menu",
@@ -34,6 +56,19 @@
 										"order.php",
 										"checkout.php",
 										"login.php?logout=true"
+										)
+						) :  array(
+						"display" => array(
+										"Home",
+										"Menu",
+										"Pay",
+										"Login"
+										),
+						"link" => array(
+										"index.php",
+										"order.php",
+										"checkout.php",
+										"login.php"
 										)
 						);
 			
