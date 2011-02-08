07aee7d7d1ea500a071dafc48934e71e6a821b53
Tom Randolph <websites@thomasrandolph.info>
2011-02-08 00:47:26 -0600

    Authentication model classes

---
+++ model/Role.php
@@ -0,0 +1,41 @@
+<?php
+	require_once('connect.php');
+	
+	class Role
+	{
+		public static function getByID($id)
+		{
+			global $db;
+			$roleSQL = "SELECT * FROM roles WHERE roleid=?";
+			$values = array($id);
+			$role = $db->qwv($roleSQL, $values);
+			
+			return Role::wrap($role);
+		}
+		
+		public static function wrap($roles)
+		{
+			$roleList = array();
+			foreach( $roles as $role )
+			{
+				array_push($roleList, new Role($role));
+			}
+			
+			return $roleList;
+		}
+		
+		private $roleid;
+		private $role;
+		
+		public function __construct($role)
+		{
+			$this->roleid = $role['roleid'];
+			$this->role = $role['role'];
+		}
+		
+		public function __get($var)
+		{
+			return $this->$var;
+		}
+	}
+?>
