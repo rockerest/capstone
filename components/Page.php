a965885684120102a78dc6fa2bf330ebfb32c36a
Tom Randolph <websites@thomasrandolph.info>
2011-02-08 20:43:15 -0600

    Revert "Separated login.php into component, modified page"
    This reverts commit 3da50822b31f0f3669832c7e1ed20a919cb68fd6.

--- components/Page.php
+++ components/Page.php
@@ -5,7 +5,6 @@
 	require_once('Header.php');	
 	require_once('MenuItem.php');
 	require_once('Session.php');
-	require_once('Login.php');
 	setSession(0,"/");
 	
 
@@ -14,7 +13,6 @@
 		protected $header;
 		protected $menuItem;
 		protected $ingredients_list;
-		protected $login;
 		private $curr;
 		private $page_title;
 
@@ -30,19 +28,18 @@
             $this->menu = new Menubar();
 			$this->header = new Header($this->curr);
 			$this->menuItem = new MenuItem(isset($_GET['id']) ? $_GET['id'] : 1);
-			$this->login = new Login();
             $this->menu->run();
 			$this->header->run();			
 			$this->menuItem->run();
         }
 
-        public function build($appContent)
-		{
+        public function build($appContent) {
             $tmpl = new Template();
+			
 			$tmpl->headerContent = $this->header->generate();
             $tmpl->menuContent = $this->curr == 1 ? $this->menu->generate() : "";			
 			$tmpl->menuItem = ($this->curr == 1) ? $this->menuItem->generate($this->curr) : "";
-            $tmpl->appContent =  $this->curr == 4 ? $this->login->generate() : $appContent;
+            $tmpl->appContent = $appContent;
 			$tmpl->title = $this->page_title;
 
             return $tmpl->build('page.html');
--- login.php
+++ login.php
@@ -3,15 +3,57 @@
 	
 	require_once('Page.php');
 	require_once('Template.php');
+	require_once('Database.php');
+	require_once('capstone.db');
 	
-	$page = new Page(4, "OrderUp - Login");
+	if(isset($_GET['logout']&&$_GET['logout']==true)
+	{
+		session_destroy();
+		header('Location: index.php');
+	}
+	
+	$page = new Page(0, "OrderUp - a new way to improve your dining experience");
+	$submit = isset( $_GET['submit'] ) ? $_GET['submit'] : 0;
+	$db = new Database($user, $pass, $dbname, $host, 'mysql');
+	
 	$tmpl = new Template();
 	
 	$page->run();
 	
-	$html = $tmpl->build('l.html');
-	$css = $tmpl->build('l.css');
-	$js = $tmpl->build('l.js');
+
+	
+	if($submit != 1)
+	{
+		$html = $tmpl->build('login.html');
+		$css = $tmpl->build('login.css');
+		//$js = $tmpl->build('index.js');
+	}
+	else
+	{
+		if($_POST['email']!=''&&$_POST['pass']!='')
+		{
+			$email = $_POST['email'];
+			$userpass = $_POST['pass'];
+
+			$sql = 'SELECT salt FROM authentication WHERE identity=?';
+			$results = $db->qwv( $sql, array($email) );
+			$salt = $results[0]['salt'];
+			$salted = hash('whirlpool', $salt.$userpass);
+			$sql = 'SELECT * FROM authentication WHERE identity=? AND password=?';
+			$values = array( $email, $salted );
+			$results = $db->qwv( $sql, $values );
+			$num = count($results);
+			if($num == 1)
+			{
+				$userid = $results[0]['userid'];
+				$user_info = $db->q("SELECT * FROM users WHERE userid = '$userid'");
+				
+				setSessionVar('active', true);
+				setSessionVar('role', $results[0]['roleid']);
+				setSessionVar('userid', $userid);
+			}
+		}
+	}
 
 	$appContent = array(
 						'html'	=>	$html,
