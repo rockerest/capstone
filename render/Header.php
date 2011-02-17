05d08cf810076b7433b5fb9191f9abff263a27b2
Tom Randolph <websites@thomasrandolph.info>
2011-02-17 15:24:32 -0600

    Matthew 3:12 - NIV

---
+++ render/Header.php
@@ -0,0 +1,98 @@
+<?php
+	set_include_path('backbone:components:content:scripts:styles');
+	
+	require_once('Template.php');
+	require_once('Session.php');
+	setSession(0,"/");
+	
+	class Header
+	{
+		private $cur_page;
+		
+		public function __construct($cur_page)
+		{
+			$this->cur_page = $cur_page;
+		}
+		
+		public function run()
+		{
+		}
+		
+		public function generate()
+		{
+			$tmpl = new Template();
+			$tmpl->curr = $this->cur_page;
+			
+			if( $_SESSION['active'] )
+			{
+				if( $_SESSION['role'] == 1 )
+				{
+					$tmpl->menu = array(
+										"display" => array(
+														"Home",
+														"Menu",
+														"Pay",
+														"OrderList",
+														"DBTEST",
+														"Item Upload",
+														"Create Users",
+														"Logout"
+														),
+										"link" => array(
+														"index.php",
+														"order.php",
+														"checkout.php",
+														"_demo/orderlist.php",
+														"_demo/dbtest.php",
+														"_demo/uploadForm.php",
+														"login.php?action=register",
+														"login.php?action=logout"
+														)
+										)
+				}
+				else
+				{
+					$tmpl->menu = array(
+										"display" => array(
+														"Home",
+														"Menu",
+														"Pay",
+														"Logout"
+														),
+										"link" => array(
+														"index.php",
+														"order.php",
+														"checkout.php",
+														"login.php?action=logout"
+														)
+										)
+				}
+			}
+			else
+			{
+				$tmpl->menu = array(
+									"display" => array(
+													"Home",
+													"Menu",
+													"Pay",
+													"Login"
+													),
+									"link" => array(
+													"index.php",
+													"order.php",
+													"checkout.php",
+													"login.php"
+													)
+									);
+			}
+			
+			$css = $tmpl->build('header.css');
+			$html = $tmpl->build('header.html');
+			//$js = $tmpl->build('header.js');
+			
+			$content = array('html' => $html, 'css' => $css, 'js' => $js);
+			return $content;
+		}
+	}
+
+?>
--- render/Page.php
+++ render/Page.php
@@ -1,9 +1,7 @@
 <?php
-	set_include_path('global:jquery:backbone:components:content:scripts:styles:images');
+	set_include_path('global:jquery:backbone:components:content:render:model:scripts:styles:images');
 	require_once('Template.php');
-	require_once('Menubar.php');
 	require_once('Header.php');	
-	require_once('MenuItem.php');
 	require_once('Session.php');
 	require_once('Login.php');
 	setSession(0,"/");
@@ -12,8 +10,6 @@
     class Page {
 		protected $menu;
 		protected $header;
-		protected $menuItem;
-		protected $ingredients_list;
 		protected $login;
 		private $curr;
 		private $page_title;
@@ -27,21 +23,14 @@
 
 		public function run()
 		{
-            $this->menu = new Menubar();
 			$this->header = new Header($this->curr);
-			$this->menuItem = new MenuItem(isset($_GET['id']) ? $_GET['id'] : 1);
-			$this->login = new Login();
-            $this->menu->run();
-			$this->header->run();			
-			$this->menuItem->run();
+			$this->header->run();
         }
 
         public function build($appContent)
 		{
             $tmpl = new Template();
 			$tmpl->headerContent = $this->header->generate();
-            $tmpl->menuContent = $this->curr == 1 ? $this->menu->generate() : "";			
-			$tmpl->menuItem = ($this->curr == 1) ? $this->menuItem->generate($this->curr) : "";
             $tmpl->appContent =  $this->curr == 4 ? $this->login->generate() : $appContent;
 			$tmpl->title = $this->page_title;
 
