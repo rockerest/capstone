76c31d3066849d4f4bc595752a5c2b0647b157db
Tom Randolph <websites@thomasrandolph.info>
2011-02-17 17:35:33 -0600

    Getting various things back to working the way they were before.  Still a long way to go.

--- components/Login.php
+++ components/Login.php
@@ -3,44 +3,113 @@
 	
 	require_once('RedirectBrowserException.php');
 	require_once('Authentication.php');
+	require_once('User.php');
 	require_once('Session.php');
 	setSession(0, '/');
+	
+	$action = isset($_POST['action']) ? $_POST['action'] : null;
 	
 	$password = isset($_POST['pass']) ? $_POST['pass'] : null;
 	$identity = isset($_POST['email']) ? $_POST['email'] : null;
 	
-	if( $password != null && $identity != null )
+	$fname = isset($_POST['fname']) ? $_POST['fname'] : null;
+	$lname = isset($_POST['lname']) ? $_POST['lname'] : null;
+	$identity = isset($_POST['email']) ? $_POST['email'] : null;
+	$ident_conf = isset($_POST['email_confirm']) ? $_POST['email_confirm'] : null;
+	$password = isset($_POST['pass']) ? $_POST['pass'] : null;
+	$pass_conf = isset($_POST['pass_confirm']) ? $_POST['pass_confirm'] : null;
+	$role = isset($_POST['register_role']) ? $_POST['register_role'] : 3;
+	
+	if( $action == 'login' )
 	{
-		$tmp = Authentication::validate($identity, $password);
-		
-		if( $tmp )
+		if( $password != null && $identity != null )
 		{
-			$user = $tmp[0];
-			setSessionVar('active', true);
-			setSessionVar('fname', $user->fname);
-			setSessionVar('lname', $user->lname);
-			setSessionVar('roleid', $user->authentication[0]->role[0]->roleid);
-			setSessionVar('userid', $user->userid);
+			$tmp = Authentication::validate($identity, $password);
 			
-			kick( null, 0 );
+			if( $tmp )
+			{
+				$user = $tmp[0];
+				setSessionVar('active', true);
+				setSessionVar('fname', $user->fname);
+				setSessionVar('lname', $user->lname);
+				setSessionVar('roleid', $user->authentication[0]->role[0]->roleid);
+				setSessionVar('userid', $user->userid);
+				
+				kick(2, null, 0 );
+			}
+			else
+			{
+				kick(0, array('identity' => $identity), 1);
+			}
 		}
 		else
 		{
-			kick($identity, 1);
+			kick(0, array('identity' => $identity), 2);
+		}
+	}
+	elseif( $action == 'register' )
+	{
+		$vals = array(	'identity' => $identity,
+						'fname' => $fname,
+						'lname' => $lname
+						);
+		if( $fname == null || $fname == "" )
+		{
+			kick(1, $vals, 0);
+		}
+		
+		if( $lname == null || $lname == "" )
+		{
+			kick(1, $vals, 1);
+		}
+		
+		if( $identity == null || $identity == "" )
+		{
+			kick(1, $vals, 2);
+		}
+		
+		if( $password == null || $password == "" )
+		{
+			kick(1, $vals, 3);
+		}
+		
+		if( $identity !== $ident_conf )
+		{
+			kick(1, $vals, 4);
+		}
+		
+		if( $password !== $pass_conf )
+		{
+			kick(1, $vals, 5);
+		}
+		
+		$stat = User::add($fname, $lname, $identity, $password, $role);
+		
+		if( $stat )
+		{
+			kick(2, null, 1);
+		}
+		else
+		{
+			kick(1, $vals, 6);
 		}
 	}
 	else
 	{
-		kick( $identity, 2);
+		kick(0, array('identity' => ""), 3);
 	}
 	
-	function kick($identity = null, $code)
+	function kick($page, $input, $code)
 	{
-		if( $identity != null )
+		if( $page == 0 )
 		{
-			throw new RedirectBrowserException("/login.php?code=" . $code . "&identity=" . $identity);
+			throw new RedirectBrowserException("/login.php?code=" . $code . "&identity=" . $input['identity']);
 		}
-		else
+		elseif( $page == 1 )
+		{
+			throw new RedirectBrowserException("/login.php?action=register&code=" . $code . "&identity=" . $input['identity'] . "&fname=" . $input['fname'] . "&lname=" . $input['lname']);
+		}
+		else( $page == 2 )
 		{
 			throw new RedirectBrowserException("/index.php?code=" . $code);
 		}
---
+++ components/Order.php
@@ -0,0 +1,44 @@
+<?php
+	set_include_path('../model:../backbone');
+	
+	require_once('RedirectBrowserException.php');
+	require_once('Session.php');
+	setSession(0, '/');
+	
+	//No DB Connection.  THere will be a model class for adding orders soon
+	
+	//get time submitted ------ //use time() - TOM
+	$date = new DateTime();
+
+	//order table vars
+	$tableid = 1;
+	$userid = $_SESSION['userid'];
+	$statusid = 1;
+	$time = $date->getTimestamp();
+	$specialcomment = $_POST['message'];
+
+	//order_items table vars
+	$orderid;
+	$item_id = $_POST['itemid'];
+	$isCustomized = 0;
+
+	//check to see if order exists -- implement when we have more users/ability to complete orders
+	$sql = "SELECT * FROM orders WHERE userid='".$userid."' AND status<>4";
+
+	$sql = "INSERT INTO orders (tableid, userid, statusid, time, specialComment) VALUES (?,?,?,?,?)";
+	$values = array($tableid, $userid, $statusid, $time, $specialcomment);
+	$db->qwv($sql, $values);
+	if( $db->stat() )
+	{
+		$orderid = $db->last();
+	}
+
+	//insert into order items
+	$sql = "INSERT INTO order_items (orderid, itemid, isCustomized) VALUES (?,?,?)";
+	$values = array($orderid, $item_id, $isCustomized);
+	$db->qwv($sql, $values);
+	if( $db->stat() )
+	{
+		$order_item_id = $db->last();
+	}
+?>
