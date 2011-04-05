63374dc7071bce5538ee349748c74d1bf6921bf4
Tom Randolph <websites@thomasrandolph.info>
2011-04-05 18:29:11 -0500

    Rendered footer.

---
+++ render/Footer.php
@@ -0,0 +1,29 @@
+<?php
+	require_once('Template.php');
+	require_once('Session.php');
+	setSession(0,"/");
+	
+	class Footer
+	{
+		public function __construct()
+		{
+		}
+		
+		public function run()
+		{
+		}
+		
+		public function generate()
+		{
+			$tmpl = new Template();
+			
+			$css = $tmpl->build('footer.css');
+			$html = $tmpl->build('footer.html');
+			//$js = $tmpl->build('footer.js');
+			
+			$content = array('html' => $html, 'css' => array('code' => $css, 'link' => 'footer'), 'js' => $js);
+			return $content;
+		}
+	}
+
+?>
--- render/Page.php
+++ render/Page.php
@@ -1,11 +1,13 @@
 <?php
 	require_once('Template.php');
-	require_once('Header.php');	
+	require_once('Header.php');
+	require_once('Footer.php');
 	require_once('Session.php');
 	setSession(0,"/");
 
     class Page {
 		protected $header;
+		protected $footer;
 		
 		private $curr;
 		private $page_title;
@@ -19,8 +21,10 @@
 		public function run()
 		{
 			$this->header = new Header($this->curr);
+			$this->footer = new Footer();
 			
 			$this->header->run();
+			$this->footer->run();
         }
 
         public function build($appContent)
@@ -28,6 +32,7 @@
             $tmpl = new Template();
 			$tmpl->headerContent = $this->header->generate();
             $tmpl->appContent = $appContent;
+			$tmpl->footerContent = $this->footer->generate();
 			$tmpl->title = $this->page_title;
 
             return $tmpl->build('page.html');
