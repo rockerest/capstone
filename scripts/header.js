1ccb6e6a831d38951eeb9464e1d6f552db6a3f23
Tom Randolph <websites@thomasrandolph.info>
2011-04-06 07:20:51 -0500

    autohide admin/manager nav items.

--- scripts/header.js
+++ scripts/header.js
@@ -1,9 +1,13 @@
 $( '#hide-1' ).click(function(){
 	$( '.rid-1' ).toggle();
+	$(this).toggleClass('okay');
 	$(this).toggleClass('error');
 });
 
 $( '#hide-2' ).click(function(){
 	$( '.rid-2' ).toggle();
+	$(this).toggleClass('okay');
 	$(this).toggleClass('error');
 });
+
+$( '.rid-2, .rid-1' ).hide();
