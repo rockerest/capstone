5aca0a9264e783056b238f9fde2bdd6e08c0dc41
Tom Randolph <websites@thomasrandolph.info>
2011-04-13 00:27:13 -0500

    adding and deleting items.
    THAT'S RIGHT.  THIS M&*^#%FU#%!* IS DONE!
    
    There are probably bugs :(

---
+++ components/DeleteItem.php
@@ -0,0 +1,32 @@
+<?php
+	set_include_path('../backbone:../global:../jquery:../components:../content:../images:../model:../render:../scripts:../styles');
+	require_once('RedirectBrowserException.php');
+	require_once('Item.php');
+
+	require_once('Session.php');
+	setSession(0, '/');
+	
+	$id = isset($_GET['id']) ? $_GET['id'] : -1;
+
+	if( !isset($_SESSION['active']) || !$_SESSION['active'] || $_SESSION['roleid'] > 2 )
+	{
+		$urlenc = urlencode("components/DeleteItem.php?id=$id");
+		throw new RedirectBrowserException("../login.php?code=10&fwd=$urlenc");
+	}
+	else
+	{
+		$item = Item::getByID($id);
+		if( $item instanceof Item )
+		{
+			$res = $item->delete();
+			if( $res )
+			{
+				throw new RedirectBrowserException("../item.php?code=1");
+			}
+			else
+			{
+				throw new RedirectBrowserException("../item.php?code=2&id=$id");
+			}
+		}
+	}
+?>
--- scripts/item.js
+++ scripts/item.js
@@ -12,8 +12,108 @@ $( 'a.n' ).click(function(){
 	return false;
 });
 
-$( '#add_ing,#add_char' ).click(function(){
-	var inp = $( this ).prev();
-	inp.after(inp.clone().val(''));
+function split( val ) {
+			return val.split( /,\s*/ );
+		}
+		
+function extractLast( term ) {
+	return split( term ).pop();
+}
+	
+var makeSuggest = function( obj, which, limit, show, insert, complex)
+{
+	var url = "";
+	switch( which )
+	{
+		case 0:
+				url = "components/suggItem.php";
+				break;
+		case 1:
+				url = "components/suggCat.php";
+				break;
+		case 2:
+				url = "components/suggIng.php";
+				break;
+		case 3:
+				url = "components/suggChar.php";
+				break;
+		default:
+				url = "";
+				break;
+	}
+	
+	obj
+		// don't navigate away from the field on tab when selecting an item
+		.bind( "keydown", function( event ) {
+			if ( event.keyCode === $.ui.keyCode.TAB &&
+					$( this ).data( "autocomplete" ).menu.active ) {
+				event.preventDefault();
+			}
+			
+			if ((event.keyCode || event.which) == 13)
+			{
+				event.preventDefault();
+			}
+		})
+		.autocomplete({
+			source: function( request, response ) {
+				$.getJSON( url, {
+					q: extractLast( request.term )
+				}, response );
+			},
+			focus: function( event, ui ) {
+				return false;
+			},
+			select: function( event, ui ) {
+				var terms = split( this.value );
+				if( terms.length <= limit )
+				{
+					// remove the current input
+					terms.pop();
+					// add the selected item
+					terms.push( ui.item[insert] );
+				}
+				else
+				{
+					terms.pop();
+				}
+				
+				if( terms.length < limit )
+				{
+					// add placeholder to get the comma-and-space at the end
+					terms.push( "" );
+				}
+				
+				this.value = terms.join( ", " );
+				return false;
+			},
+			delay: 100
+		}).data( "autocomplete" )._renderItem = function( ul, item ) {
+			return $( "<li></li>" )
+				.data( "item.autocomplete", item )
+				.append( function(){
+					if( complex == 1 )
+					{
+						return "<a>" + item[show] + " : " + item[insert] + "</a>";
+					}
+					else if( complex == 0 )
+					{
+						return "<a>" + item[show] + "</a>";
+					}
+				})
+				.appendTo( ul );
+		};
+}
+
+if( $('#add').length > 0 )
+{
+	makeSuggest($('#add #name'), 0, 1, "name", "name", 0);
+	makeSuggest($('#add #cat'), 1, 1, "name", "name", 0);
+	makeSuggest($('#add #chars'), 3, 10000, "name", "name", 0);
+	makeSuggest($('#add #ings'), 2, 10000, "name", "name", 0);
+}
+
+$( '#delete #no' ).click(function(){
+	history.go(-1);
 	return false;
 });
