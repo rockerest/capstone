513910fb781bdea38162061d4f6f8d77faf28837
Tom Randolph <websites@thomasrandolph.info>
2011-04-20 00:05:01 -0500

    Move redirect testing to the top of Header->render to save DB calls.
    Allow table override.
    
    Soften default text color.

--- render/Header.php
+++ render/Header.php
@@ -23,6 +23,43 @@
 			$tmpl = new Template();
 			$tmpl->curr = $this->cur_page;
 			
+			if( $_SERVER['QUERY_STRING'] != null )
+			{
+				$qs = $_SERVER['QUERY_STRING'];
+				$st = explode("&", $qs);
+				$vals = array();
+				foreach($st as $string)
+				{
+					$pair = explode("=", $string);
+					$vals[$pair[0]] = $pair[1];
+				}
+				
+				//array of available testing tables
+				$tables = array(1);				
+				
+				if( in_array(hexdec($vals['hor']) / 1000, $tables) )
+				{
+					$_SESSION['umbrella']['tableid'] = hexdec($vals['hor'] / 1000);
+					header('Location: index.php');
+				}
+			}
+			
+			if( $_SERVER['SCRIPT_NAME'] != '/login.php' && !isset($_SESSION['umbrella']['tableid']) )
+			{
+				if( $_SESSION['roleid'] > 2 )
+				{
+					$loc = urlencode("login.php?code=11");
+					header('Location: login.php?action=logout&fwd='.$loc);
+				}
+				else
+				{
+					if( $_SERVER['SCRIPT_NAME'] != '/table.php' )
+					{
+						header('Location: table.php?code=0');
+					}
+				}
+			}
+			
 			if( $_SESSION['active'] )
 			{
 				$tmpl->menu = Navigation::getByRole($_SESSION['roleid']);
@@ -46,22 +83,6 @@
 														4,
 														0
 														));
-			}
-			
-			if( $_SERVER['SCRIPT_NAME'] != '/login.php' && !isset($_SESSION['umbrella']['tableid']) )
-			{
-				if( $_SESSION['roleid'] > 2 )
-				{
-					$loc = urlencode("login.php?code=11");
-					header('Location: login.php?action=logout&fwd='.$loc);
-				}
-				else
-				{
-					if( $_SERVER['SCRIPT_NAME'] != '/table.php' )
-					{
-						header('Location: table.php?code=0');
-					}
-				}
 			}
 			
 			$css = $tmpl->build('header.css');
