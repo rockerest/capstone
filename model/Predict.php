1faeb3f301ae893cbf665d8f60f45795823a00d5
Tom Randolph <websites@thomasrandolph.info>
2011-04-27 21:40:18 -0500

    My brain is already overloaded. Does anyone have any LSD I could drop to get a handle on this thing?

--- model/Predict.php
+++ model/Predict.php
@@ -2,8 +2,9 @@
 	require_once('connect.php');
 	
 	require_once('Item.php');
+	require_once('Rating.php');
 
-	class Predict
+	class Predict extends Base
 	{
 		private $user;
 		public function __construct($user)
@@ -11,32 +12,71 @@
 			$this->user = $user;
 		}
 		
-		public function users()
+		public function compare($one, $two)
 		{
-		}
-		
-		public function items()
-		{
-		}
-		
-		public function rating( $item )
-		{
-			if( $item instanceof Item )
+			if( !($one instanceof Item) )
 			{
-				$it = $item;
+				if( is_integer($one) )
+				{
+					$one = Item::getByID($one);
+				}
+				else
+				{
+					return false;
+				}
 			}
-			elseif( is_integer( $item ) )
+			
+			if( !($two instanceof Item) )
 			{
-				$it = Item::getByID( $item );
+				if( is_integer($two) )
+				{
+					$two = Item::getByID($two);
+				}
+				else
+				{
+					return false;
+				}
+			}
+			
+			$ratings[0] = Rating::getByUserForItem($this->user->userid, $one->itemid);
+			$ratings[1] = Rating::getByUserForItem($this->user->userid, $two->itemid);
+			$views[0] = View::getByUserForItem($this->user->userid, $one->itemid);
+			$views[1] = View::getByUserForItem($this->user->userid, $two->itemid);
+			
+			//first, try to compare ratings
+			if( $ratings[0] || $ratings[1] )
+			{
+				if( $ratings[0] && $ratings[1] )
+				{
+				}
+				else
+				{
+					//one item doesn't have ratings
+				}
 			}
 			else
 			{
-				return false;
+				//if the user has no ratings, try to compare number of views
+				//people are more likely to view something if they want it (even subconsciously)
+				//so the comparison of views could offer a rough estimate
+				if( $views[0] || $views[1] )
+				{
+					if( $views[0] && $views[1] )
+					{
+					}
+					else
+					{
+						//one of the items hasn't been viewed
+					}
+				}
+				else
+				{
+					//The user has no ratings for either item, and no views for either item.
+					//The only option is to return an unknown for the direct comparison
+				}
 			}
-		}
-		
-		private function normalize( $chars )
-		{
+			
+			
 		}
 	}
 ?>
