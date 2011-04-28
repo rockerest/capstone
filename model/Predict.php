ad6caae7c0a960294f83d1bbc5d8b633dee1faa9
Tom Randolph <websites@thomasrandolph.info>
2011-04-27 23:19:41 -0500

    Given an item, get all items that might be similar and calculate the similarity of each.
    TOTALLY UNTESTED!  TRUST THIS COMPLETELY AND USE IT IN YOUR CODE!

--- model/Predict.php
+++ model/Predict.php
@@ -2,11 +2,109 @@
 	require_once('connect.php');
 	
 	require_once('Item.php');
+	require_once('View.php');
 	require_once('Rating.php');
 
 	class Predict extends Base
 	{
+		public static function similar($item)
+		{
+			//make sure $item is an object
+			if( !($item instanceof Item) )
+			{
+				if( is_integer($item) )
+				{
+					$item = Item::getByID($item);
+				}
+				else
+				{
+					return false;
+				}
+			}
+			
+			$CHARADD = .1;//each matching characteristic = .1
+			$CATADD = .05;//each matching category level = .05
+			$VOADD = .025;//a user viewed one and ordered other = .025
+			
+			$UGTMADD = -.5;//more unmatched characteristics than matched = -.5
+			$NOMATCHTOPLVLADD = -.25;//top level category doesn't match = -.25
+			
+			$chars = Base::toArray($item->characteristics);
+			$views = Base::toArray(View::getByItem($item->itemid));
+			
+			//yes that's right, go get ALL of the items that have ANY of the characteristics
+			$items = array();
+			foreach( $chars as $char )
+			{
+				$itms = Base::toArray(Item::getByCharacteristic($char->characteristicid));
+				$items = array_merge($items, $itms);
+			}
+			
+			//now go get all the items in the category chain
+			$numbers = explode('.', $item->category->number);
+			$itms = array();
+			while( count($numbers) > 0 )
+			{
+				$newtms = Base::toArray(Item::getByCategorySearch(implode('.',$numbers).'%')); //mmm, delicious Newtms.
+				$itms = array_merge($itms, array_diff($newtms, $itms));
+				array_pop($numbers);
+			}
+			
+			//itemset is all chars + all categories
+			$items = array_merge($items, $itms);
+			$similarities = array();
+			foreach( $items as $itm )
+			{
+				$charcount['num'] = 0;
+				$charcount['match'] = 0;
+				$itch = Base::toArray($itm->characteristics);
+				foreach( $itch as $ch )
+				{
+					if( in_array($ch, $chars) )
+					{
+						$charcount['match']++;
+					}
+					$charcount['num']++;
+				}
+				
+				$itmNum = explode('.', $item->category->number);
+				$thisNum = explode('.', $itm->category->number);
+				$catcount['topmatch'] = false;
+				$catcount['num'] = 0;
+				$catcount['match'] = 0;
+				for($i = 0; $i<count($itmNum); $i++)
+				{
+					if( $itmNum[$i] == $thisNum[$i] )
+					{
+						if( $i == 0 )
+						{
+							$catcount['topmatch'] = true;
+						}
+						$catcount['match']++;
+					}
+					$catcount['num']++;
+				}
+				
+				$similarity = 0;
+				if( ($charcount['num'] / 2) > $charcount['match'] )
+				{
+					$similarity += $UGTMADD;
+				}
+				if( !$catcount['topmatch'] )
+				{
+					$similarity += $NOMATCHTOPLVLADD;
+				}
+				$similarity += ($CATADD * $catcount['match']);
+				$similarity += ($CHARADD * $charcount['match']);
+				
+				$similarities = array_merge($similarities, array($itm->itemid => array("characteristic" => $charcount, "category" => $catcount, "similarity" => $similarity)));
+			}
+			
+			return $similarities;
+		}
+		
 		private $user;
+		
 		public function __construct($user)
 		{
 			$this->user = $user;
@@ -75,8 +173,10 @@
 					//The only option is to return an unknown for the direct comparison
 				}
 			}
-			
-			
+		}
+		
+		public function recommend()
+		{
 		}
 	}
 ?>
